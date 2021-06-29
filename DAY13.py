4fc93dc3c3327ef994d6da499fb78978cbd3b9e6
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2021-06-29 21:17:44 +0530

    Create DAY13.py

---
+++ DAY13.py
@@ -0,0 +1,77 @@
+# i = 1
+# sum = 0
+# n = 10
+
+# while i<=n:
+#     sum = sum +i  #1...#3...#55
+#     i+=1
+
+# print("The sum is :", sum)
+
+
+# multiplication table of 24 
+# c=1
+# while c<=10:
+#     print(f'24 X {c} = {24*c}')
+#     c+=1
+
+
+
+# l = [ 1, 2, 3, 4, 56, 9 ] 
+# c = 0
+# while c <= (len(l)-1):
+#     print(l[c],end=" ")
+#     c+=1
+
+
+# string = "Python is the best!"
+# e and t  should not print
+
+# current letter is : P
+# ------------------: y
+
+# s= 'Python is best'
+# print(len(s))
+# c=0
+# while c<len(s):
+#     c+=1
+#     pass
+# print(f'Current value : {c}')
+
+#for loop
+
+# for i in range(0,21,2):
+#     print(i)
+
+# string = ["apple","mango","banana"]
+# integer = [1,2,3]
+# # for i in string:
+# #     # print(i)
+# #     if i == 1:
+# #         continue
+# #     print(i)
+# for i in string:
+#     for j in integer:
+#         print(i,j)
+
+list1 = [150, 12, 15, 32, 42, 55, 75, 122, 132,180,150, 200]
+# val divisible by 5.....bt that value must comes before 150
+
+# for i in list1:
+    # if i%5==0 and i<150:
+    #     print(i)
+    #     i+=1
+
+# numb = 45876
+# print...how many digits
+# num= int( input("Enter a number: ") )
+# count= 0
+# while(num !=0) :
+#     num //= 10 
+#     count+= 1
+ 
+# print(f"No. of digits: {count}")
+
+
+num = 0
+print(num//10)
